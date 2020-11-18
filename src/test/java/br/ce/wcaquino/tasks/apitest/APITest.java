69a9d477ebb365f291c10196ce5354d6ed790352
Your Name <you@example.com>
2020-11-18 08:57:48 -0300

    .

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -11,7 +11,7 @@ public class APITest {
 	
 	@BeforeClass
 	public static void setup() {
-		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+		RestAssured.baseURI = "http://tomcat:8001/tasks-backend";
 	}
 
 	@Test
